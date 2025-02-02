9f0483ba5c5051b55456e00c7459245e5587df36
jibran <jibrankhot5@gmail.com>
2025-02-02 20:31:23 +0530

    property-card added and view changes

--- src/app/property/property-list/property-list.component.ts
+++ src/app/property/property-list/property-list.component.ts
@@ -3,8 +3,33 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-property-list',
   templateUrl: './property-list.component.html',
-  styleUrls: ['./property-list.component.css']
+  styleUrls: ['./property-list.component.css'],
 })
 export class PropertyListComponent {
-
+  Property: any = [
+    {
+      Id: 1,
+      Name: 'Birla House',
+      Type: 'House',
+      Price: 120000,
+    },
+    {
+      Id: 2,
+      Name: 'Erose Villa',
+      Type: 'Villa',
+      Price: 420000,
+    },
+    {
+      Id: 3,
+      Name: 'Macro Home',
+      Type: 'House',
+      Price: 220000,
+    },
+    {
+      Id: 4,
+      Name: 'Pearl White House',
+      Type: 'House',
+      Price: 360000,
+    },
+  ];
 }
