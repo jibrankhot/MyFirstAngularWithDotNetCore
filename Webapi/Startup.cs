6e812627977a8fe4140cf96a732f37e9d4cf55e3
jibran <jibrankhot5@gmail.com>
2025-05-22 16:52:54 +0530

    Clean up: apply .gitignore and remove unnecessary tracked files

---
+++ Webapi/Startup.cs
@@ -0,0 +1,84 @@
+using System.Text;
+using AutoMapper;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using WebAPI.Data;
+using WebAPI.Extensions;
+using WebAPI.Helpers;
+using WebAPI.Interfaces;
+using WebAPI.Services;
+
+namespace WebAPI
+{
+    public class Startup
+    {
+    public Startup(IConfiguration configuration)
+    {
+      Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+      var connectionString = Configuration.GetConnectionString("Default");
+
+            
+            services.AddDbContext<DataContext>(options => 
+            options.UseSqlServer(connectionString));
+            services.AddControllers().AddNewtonsoftJson();
+            services.AddCors();
+            services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<IPhotoService, PhotoService>();
+
+            var secretKey = Configuration.GetSection("AppSettings:Key").Value;
+            var key = new SymmetricSecurityKey(Encoding.UTF8
+                .GetBytes(secretKey));
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            // services.AddAuthentication("Bearer")
+                .AddJwtBearer(opt => {
+                    opt.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                        IssuerSigningKey = key                        
+                    };
+                });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.ConfigureExceptionHandler(env);            
+
+            // app.ConfigureBuiltinExceptionHandler;         
+            
+            app.UseRouting();
+            app.UseHsts();
+            app.UseHttpsRedirection();
+
+            app.UseCors(m => m.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
